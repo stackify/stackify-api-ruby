d2089bcd53297374b130dc73e1c28b2377f4a1d2
Arman Ortega <aortega@stackify.com>
2020-02-29 00:40:02 +0800

    INV-1229 - Change transport name from <default> to <log>

--- lib/stackify/utils/configuration.rb
+++ lib/stackify/utils/configuration.rb
@@ -47,7 +47,7 @@ module Stackify
 
     def validate_transport_type
       return true if ['agent_socket', 'agent_http', 'log'].include? @transport
-      @errors << 'Transport should be one of these values: [agent_socket, agent_http, default]. Should be a String.'
+      @errors << 'Transport should be one of these values: [agent_socket, agent_http, log]. Should be a String.'
     end
 
     private
@@ -58,7 +58,7 @@ module Stackify
       validate_mode_type
     end
 
-    # Perform validation if transport type is default
+    # Perform validation if transport type is log
     # Required parameters are: env, app_name, api_key, log_level
     def validate_default_transport
       validate_app_name &&
